9fd060a3b1ddc5587d1287bf511a9c4621bef3a3
Pia Lyck <pialyckgithub@gmail.com>
2017-08-21 12:22:48 +1000

    Add comments for including bundle.js and the numeral library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import numeral from 'numeral';
+import numeral from 'numeral'; //library to handle number (from package.json)
 
-const totalWorth = numeral(10000000).format('$0,0.00');
-console.log(`I am an amazing creature and my worth is at least ${totalWorth}!`);
+const totalWorth = numeral(1200425).format('$0,0.00');
+console.log(`I am an amazing creature and my worth is at least ${totalWorth}!`); //using backticks (clever ES6 template string feature)
